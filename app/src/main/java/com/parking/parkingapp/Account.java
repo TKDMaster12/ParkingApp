edbecc3d0fdb33e7fee798673da5f37db4e5fa4f
TKDMaster12 <bierstephen@gmail.com>
2018-11-18 20:44:29 -0500

    add account file

--- app/src/main/java/com/parking/parkingapp/Account.java
+++ app/src/main/java/com/parking/parkingapp/Account.java
@@ -1,13 +1,120 @@
 package com.parking.parkingapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Account extends AppCompatActivity {
+
+    @BindView(R.id.CameraBtn)
+    Button CameraBtn;
+    @BindView(R.id.iv)
+    ImageView imageView;
+
+    private static final String TAG = "CapturePicture";
+    static final int REQUEST_PICTURE_CAPTURE = 1;
+    private String pictureFilePath;
+    private String deviceIdentifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        ButterKnife.bind(this);
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            CameraBtn.setEnabled(false);
+        }
+
+        getInstallationIdentifier();
+
+        CameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+                    sendTakePictureIntent();
+                }
+            }
+        });
+    }
+
+    private void sendTakePictureIntent() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_FINISH_ON_COMPLETION, true);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, REQUEST_PICTURE_CAPTURE);
+
+            File pictureFile = null;
+            try {
+                pictureFile = getPictureFile();
+            } catch (IOException ex) {
+                Toast.makeText(this,
+                        "Photo file can't be created, please try again",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (pictureFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.parking.parkingapp.fileprovider",
+                        pictureFile);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(cameraIntent, REQUEST_PICTURE_CAPTURE);
+            }
+        }
+    }
+
+    private File getPictureFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String pictureFile = "PARKING_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(pictureFile, ".jpg", storageDir);
+        pictureFilePath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PICTURE_CAPTURE && resultCode == RESULT_OK) {
+            File imgFile = new File(pictureFilePath);
+            if (imgFile.exists()) {
+                imageView.setImageURI(Uri.fromFile(imgFile));
+            }
+        }
+    }
+
+    protected synchronized String getInstallationIdentifier() {
+        if (deviceIdentifier == null) {
+            SharedPreferences sharedPrefs = this.getSharedPreferences(
+                    "DEVICE_ID", Context.MODE_PRIVATE);
+            deviceIdentifier = sharedPrefs.getString("DEVICE_ID", null);
+            if (deviceIdentifier == null) {
+                deviceIdentifier = UUID.randomUUID().toString();
+                SharedPreferences.Editor editor = sharedPrefs.edit();
+                editor.putString("DEVICE_ID", deviceIdentifier);
+                editor.commit();
+            }
+        }
+        return deviceIdentifier;
     }
 }
